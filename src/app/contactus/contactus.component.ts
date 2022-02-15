97cdd27f7479cef31328e8670f83cfab572faa30
Teaca Stefan <stefanteaca37@gmail.com>
2022-02-15 15:12:29 +0100

    completely done ,heheeheh

--- src/app/contactus/contactus.component.ts
+++ src/app/contactus/contactus.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup,FormControl,Validators } from '@angular/forms';
 @Component({
-  selector: 'app-contactus',
+  selector: 'contactus',
   templateUrl: './contactus.component.html',
   styleUrls: ['./contactus.component.css']
 })
 export class ContactusComponent implements OnInit {
-
+  info = new FormGroup({
+    name : new FormControl('',Validators.minLength(2)),
+    lastName:new FormControl('',Validators.minLength(3)),
+    eMail : new FormControl('',Validators.email),
+    age: new FormControl('',Validators.required)
+  });
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  onSubmit(){
+    if(this.info.valid)
+    var a = this.info.value;
+    console.table(a)
+  }
 }
